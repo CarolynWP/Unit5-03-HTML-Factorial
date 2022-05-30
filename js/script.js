3300e7f73b337e62672904c5057d8e94e48b2051
Carolyn Webster-Pless <99274537+CarolynWP@users.noreply.github.com>
2022-05-30 14:32:48 +0000

    Done!!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
+//use strict
 "use strict"
 
 function numberKing(){
-
+// variables
+	let userNumber = parseInt(document.getElementById("number").value);
+	let counter= 1
+	let factorial = 1
 	
+	 //do while loop for factorials
+	do {
+		factorial = factorial * counter
+		counter = counter + 1
+	} 
+		while (counter <= userNumber)
+
+	//output
+	document.getElementById("factorial").innerHTML = "YES. YES! "+ factorial + " IS SO MUCH FOOD!! WITH ENOUGH OF THIS POWER, I CAN TURN E V E R Y T H I N G INTO NUMBERS!!! MWAHAHAHAHAHAHHAHAH! I AM THE NUMBER KING!";
+	
+	document.getElementById("number-king").innerHTML = '<img src="./images/number_king.png" width="600px" alt="Number king?!">';
 }
